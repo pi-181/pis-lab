51174037673505550310ac410f4e2730257afffc
demkom58 <demkom58@gmail.com>
2021-02-19 17:29:24 +0200

    Database connection creation fix

--- connections/pis_blog.php
+++ connections/pis_blog.php
@@ -4,9 +4,8 @@ $db = "pis_blog";
 $user = "admin";
 $password = "admin";
 
-//$mysqli = mysqli_init();
-$link = mysqli_connect($localhost, $user, $password);
-//or trigger_error(mysqli_error($mysqli),E_USER_ERROR);
-
-//mysqli_query($link, "SET NAMES cp1251;") or die(mysqli_error($link));
-//mysqli_query($link, "SET CHARACTER SET cp1251;") or die(mysqli_error($link));
+$connection = mysqli_connect($localhost, $user, $password, $db);
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
