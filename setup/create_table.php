88020981db7b4c0022e73e2e6fd989d683418951
demkom58 <demkom58@gmail.com>
2021-02-19 17:27:29 +0200

    A bit change notes table

--- setup/create_table.php
+++ setup/create_table.php
@@ -7,6 +7,6 @@ $db_name = "pis_blog";
 $select = mysqli_select_db($link, $db_name);
 echo ($select ? "База даних обрана" : "База даних не обрана") . "<br>";
 
-$query = "CREATE TABLE notes (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id), created DATE, title VARCHAR (20), article VARCHAR (255))";
+$query = "CREATE TABLE notes (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id), created DATE, title VARCHAR (100), article TEXT (20000))";
 $result = mysqli_query($link, $query);
 echo ($result ? "Таблиця notes успішно створена" : "Не вдалось створити таблицю notes") . "<br>";
